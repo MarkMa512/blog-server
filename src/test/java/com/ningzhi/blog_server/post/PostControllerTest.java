28a2f38f266f46b3b04e13a80529ef6d80bfcdaf
MarkMa512 <ningzhi04@hotmail.com>
2025-01-09 22:23:21 +0800

    test: should/shouldNotFindPostById test up

--- src/test/java/com/ningzhi/blog_server/post/PostControllerTest.java
+++ src/test/java/com/ningzhi/blog_server/post/PostControllerTest.java
@@ -3,6 +3,7 @@ package com.ningzhi.blog_server.post;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @WebMvcTest(PostController.class)
 class PostControllerTest {
@@ -59,19 +61,35 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.size()",is(posts.size())));
     }
 
-//    @Test
-//    void findById() {
-//    }
-//
-//    @Test
-//    void create() {
-//    }
-//
-//    @Test
-//    void update() {
-//    }
-//
-//    @Test
-//    void delete() {
-//    }
+    @Test
+    void shouldFindOnePostById() throws Exception {
+        Post post = posts.get(0);
+        when(postRepository.findById(ArgumentMatchers.anyLong())).thenReturn(Optional.of(post));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(post.id().intValue()))) // Explicit Type Conversion needed here
+                .andExpect(jsonPath("$.title", is(post.title())))
+                .andExpect(jsonPath("$.content", is(post.content())))
+                .andExpect(jsonPath("$.author", is(post.author().intValue())))
+                .andExpect(jsonPath("$.createdAt", is(post.createdAt().withNano(post.createdAt().getNano() / 1000 * 1000).toString())))
+                .andExpect(jsonPath("$.updatedAt", is(post.updatedAt().withNano(post.updatedAt().getNano() / 1000 * 1000).toString())))
+                .andExpect(jsonPath("$.version", is(post.version())));
+    }
+    @Test
+    void shouldNotFindOnePostById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/posts/99"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
 }
